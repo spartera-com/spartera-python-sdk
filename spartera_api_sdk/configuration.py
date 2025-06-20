1cdca0749bacbcdd9278b5a4e589a9816a01a3e1
tony-spartera <tony@spartera.com>
2025-06-20 15:38:06 -0400

    Release v1.0.74
    - Auto-generated SDK from Spartera API specification
    - Updated to version 1.0.74
    - Generated on Fri Jun 20 15:38:06 EDT 2025
    
    This SDK is automatically generated from our private monorepo.
    Report issues at: https://github.com/spartera-com/spartera-python-sdk/issues

--- spartera_api_sdk/configuration.py
+++ spartera_api_sdk/configuration.py
@@ -532,7 +532,7 @@ conf = spartera_api_sdk.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.0.0\n"\
-               "SDK Package Version: 1.0.69".\
+               "SDK Package Version: 1.0.74".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
